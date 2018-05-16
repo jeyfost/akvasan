4931638d45feda8f77ce61ea36b09a06c22ad4ff
jeyfost <foster_andrew@tut.by>
2018-05-16 13:03:23 +0300

    Добавлена страница входа в панель администрирования

---
+++ admin/admin.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jeyfost
+ * Date: 16.05.2018
+ * Time: 12:59
+ */
---
+++ js/admin/common.js
@@ -0,0 +1,43 @@
+$(window).on("load", function () {
+    if($('*').is($('#leftMenu'))) {
+        if($('#leftMenu').height() < parseInt($(window).height() - $('#topLine').height())) {
+            $('#leftMenu').height(parseInt($(window).height() - $('#topLine').height()));
+        }
+    }
+
+    if($('*').is($('#content'))) {
+        $('#content').width(parseInt($(window).width() - $('#leftMenu').width() - 130));
+    }
+});
+
+$(window).on("resize", function () {
+    if($('*').is($('#leftMenu'))) {
+        if($('#leftMenu').height() < parseInt($(window).height() - $('#topLine').height())) {
+            $('#leftMenu').height(parseInt($(window).height() - $('#topLine').height()));
+        }
+    }
+
+    if($('*').is($('#content'))) {
+        $('#content').width(parseInt($(window).width() - $('#leftMenu').width() - 130));
+    }
+});
+
+function buttonHover(id, action) {
+    if(action === 1) {
+        document.getElementById(id).style.backgroundColor = "#54a9fc";
+        document.getElementById(id).style.color = "#fff";
+    } else {
+        document.getElementById(id).style.backgroundColor = "#dedede";
+        document.getElementById(id).style.color = "#4c4c4c";
+    }
+}
+
+function buttonHoverRed(id, action) {
+    if(action === 1) {
+        document.getElementById(id).style.backgroundColor = "#d33434";
+        document.getElementById(id).style.color = "#fff";
+    } else {
+        document.getElementById(id).style.backgroundColor = "#dedede";
+        document.getElementById(id).style.color = "#4c4c4c";
+    }
+}
